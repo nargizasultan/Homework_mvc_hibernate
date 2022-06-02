69a6715b639c68f31f31d3f30a14032d2aad2d82
nargiza <sultanmuratova500@gmail.com>
2022-06-02 12:06:15 +0600

    nargiza

---
+++ src/main/java/thymeleaf/controllers/MyController.java
@@ -0,0 +1,14 @@
+package thymeleaf.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class MyController {
+    @GetMapping
+    public String showFirstView(){
+        return "show-first-page";
+    }
+}
